096074cdaa140bb1187e8c5befc8bce5658aafb6
Maicon Santana <maiconpintoabreu@gmail.com>
2026-07-26 00:14:04 +0100

    Start virtual input

--- Levels/Maicon/touch_controls.gd
+++ Levels/Maicon/touch_controls.gd
@@ -1,18 +1,33 @@
 extends Control
+@onready var virtual_joystick: VirtualJoystick = $VirtualJoystick
+@onready var virtual_joystick_2: VirtualJoystick = $VirtualJoystick2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	if not DisplayServer.is_touchscreen_available():
+		virtual_joystick.hide()
+		virtual_joystick_2.hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	pass
-
-
-func _on_virtual_joystick_pressed() -> void:
-	pass # Replace with function body.
-
-
-func _on_virtual_joystick_released(_input_vector: Vector2) -> void:
-	pass # Replace with function body.
+	# TODO: if possible make it smoother later
+	if Input.is_action_pressed("look_up"):
+		var ev = InputEventMouseMotion.new()
+		# Set as ui_left, pressed.
+		ev.relative.y = -10;
+		Input.parse_input_event(ev)
+	if Input.is_action_pressed("look_down"):
+		var ev = InputEventMouseMotion.new()
+		# Set as ui_left, pressed.
+		ev.relative.y = 10;
+		Input.parse_input_event(ev)
+	if Input.is_action_pressed("look_left"):
+		var ev = InputEventMouseMotion.new()
+		# Set as ui_left, pressed.
+		ev.relative.x = -10;
+		Input.parse_input_event(ev)
+	if Input.is_action_pressed("look_right"):
+		var ev = InputEventMouseMotion.new()
+		# Set as ui_left, pressed.
+		ev.relative.x = 10;
+		Input.parse_input_event(ev)
