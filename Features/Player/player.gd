4e49da5243a945992a63019f97a33b5caa01bfb7
Michael R Ryan <mrainsfordryan@gmail.com>
2026-07-25 02:13:57 +0100

    Added main level blockout and tweaked player movement

--- Features/Player/player.gd
+++ Features/Player/player.gd
@@ -3,14 +3,16 @@ class_name Player
 
 signal on_can_interact(can_interact: bool)
 
-@export var move_speed: float = 5.0
-@export var acceleration: float = 10.0
-@export var deceleration: float = 20.0
+@export var move_speed: float = 3.2
+@export var acceleration: float = 4
+@export var deceleration: float = 6
 @export var mouse_sensitivity: float = 0.002
 @export var max_look_up: float = deg_to_rad(80)
 @export var max_look_down: float = deg_to_rad(-80)
-@export var gravity: float = -15.8
-@export var jump_speed: float = 7.0
+@export var gravity: float = 9.8
+@export var jump_height: float = 0.5
+
+@onready var jump_speed: float = sqrt(2 *  gravity * jump_height)
 
 @onready var head: Node3D = $Head
 @onready var camera: Camera3D = $Head/Camera3D
@@ -22,12 +24,16 @@ var camera_rot_y: float = 0.0
 var velocity_desired: Vector3 = Vector3.ZERO
 var last_interacted_task_object: TaskObjectBase = null
 
+
+#-------------------------------------------------------------------------------
 func _ready() -> void:
-	"""Capture mouse when game starts."""
+	# Capture mouse when game starts.
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
+
+#-------------------------------------------------------------------------------
 func _input(event: InputEvent) -> void:
-	"""Handle mouse movement for view direction."""
+	# Handle mouse movement for view direction.
 	if event is InputEventMouseMotion:
 		camera_rot_y -= event.relative.x * mouse_sensitivity
 		camera_rot_x -= event.relative.y * mouse_sensitivity
@@ -40,8 +46,10 @@ func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("exit"):
 		get_tree().quit()
 
+
+#-------------------------------------------------------------------------------
 func _process_input() -> Vector3:
-	"""Process movement direction from input."""
+	# Process movement direction from input.
 	var direction := Vector3.ZERO
 	
 	if Input.is_action_pressed("move_forward"):
@@ -54,18 +62,18 @@ func _process_input() -> Vector3:
 		direction += transform.basis.x
 	
 	return direction.normalized()
-
-func _physics_process(delta: float) -> void:
-	"""Apply movement, gravity, and acceleration each physics frame."""
-	var direction := _process_input()
+	
+	
+#-------------------------------------------------------------------------------
+# Apply movement, gravity, and acceleration each physics frame.
+func _process_movement(direction : Vector3, delta: float) -> void:
 	velocity_desired = direction * move_speed
 	
 	# Horizontal components
 	var vel_h = velocity
 	vel_h.y = 0
 	
-	var diff = velocity_desired - vel_h
-	var rate = acceleration if diff.length() > 0 else deceleration
+	var rate = acceleration if direction.length() > 0 else deceleration
 	
 	vel_h = vel_h.lerp(velocity_desired, rate * delta)
 
@@ -75,12 +83,17 @@ func _physics_process(delta: float) -> void:
 	
 	# Apply gravity
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		velocity.y -= gravity * delta
 	else:
 		if Input.is_action_just_pressed("jump"):
 			velocity.y = jump_speed
 	
 	move_and_slide()
+	
+
+#-------------------------------------------------------------------------------
+func _physics_process(delta: float) -> void:
+	_process_movement(_process_input(), delta)
 	
 	if raycast.is_colliding():
 		var collider = raycast.get_collider()
