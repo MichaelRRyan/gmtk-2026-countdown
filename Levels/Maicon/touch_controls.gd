30e408203b22ed4b8e2fbb94370c1dbaad525ec6
Maicon Santana <maiconpintoabreu@gmail.com>
2026-07-26 00:33:47 +0100

    Add interaction button for virtual input

--- Levels/Maicon/touch_controls.gd
+++ Levels/Maicon/touch_controls.gd
@@ -1,33 +1,42 @@
 extends Control
 @onready var virtual_joystick: VirtualJoystick = $VirtualJoystick
 @onready var virtual_joystick_2: VirtualJoystick = $VirtualJoystick2
+@onready var interact_button: Button = $InteractButton
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if not DisplayServer.is_touchscreen_available():
 		virtual_joystick.hide()
 		virtual_joystick_2.hide()
+		interact_button.hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	# TODO: if possible make it smoother later
 	if Input.is_action_pressed("look_up"):
 		var ev = InputEventMouseMotion.new()
-		# Set as ui_left, pressed.
 		ev.relative.y = -10;
 		Input.parse_input_event(ev)
 	if Input.is_action_pressed("look_down"):
 		var ev = InputEventMouseMotion.new()
-		# Set as ui_left, pressed.
 		ev.relative.y = 10;
 		Input.parse_input_event(ev)
 	if Input.is_action_pressed("look_left"):
 		var ev = InputEventMouseMotion.new()
-		# Set as ui_left, pressed.
 		ev.relative.x = -10;
 		Input.parse_input_event(ev)
 	if Input.is_action_pressed("look_right"):
 		var ev = InputEventMouseMotion.new()
-		# Set as ui_left, pressed.
 		ev.relative.x = 10;
 		Input.parse_input_event(ev)
+
+
+func _on_button_down() -> void:
+	var ev = InputEventAction.new()
+	ev.set_as_action("interact", true)
+	Input.parse_input_event(ev)
+
+func _on_button_up() -> void:
+	var ev = InputEventAction.new()
+	ev.set_as_action("interact", false)
+	Input.parse_input_event(ev)
